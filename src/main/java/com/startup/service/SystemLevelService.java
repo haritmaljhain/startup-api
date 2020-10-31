958fdb185cdc5efbc6ea50a1e5e72c17e88ec9ff
haritmaljhain <maljhain@gmail.com>
2020-10-31 15:46:18 +0530

    Updating Interface

--- src/main/java/com/startup/service/SystemLevelService.java
+++ src/main/java/com/startup/service/SystemLevelService.java
@@ -1,29 +1,5 @@
 package com.startup.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface SystemLevelService {
 
-import com.startup.entity.SystemLevel;
-import com.startup.repositories.SystemLevelRepository;
-
-/**
- * 
- * @author harit.kumar
- * @Service annotation used to declare the class as a Service class which holds the business logic.
- */
-
-@Service
-public class SystemLevelService {
-
-	@Autowired
-	SystemLevelRepository repository;
-	
-	public SystemLevel getCorporateDetails() {
-		
-		
-		
-		
-		
-		return null ;
-	}
 }
